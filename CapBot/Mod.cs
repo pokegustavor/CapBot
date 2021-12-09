c3a14773f3ea853a6b77e1c4367ee025f9f555b2
Gustavo Ribeiro <pokegustavo@gmail.com>
2021-12-09 13:20:01 -0300

    Adicionar arquivos de projeto.

---
+++ CapBot/Mod.cs
@@ -0,0 +1,20 @@
+﻿using PulsarModLoader;
+
+namespace CapBot
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "1.0.0";
+
+        public override string Author => "pokegustavo";
+
+        public override string ShortDescription => "Adds a bot as the captain";
+
+        public override string Name => "CapBot";
+
+        public override string HarmonyIdentifier()
+        {
+            return "pokegustavo.CapBot";
+        }
+    }
+}
---
+++ CapBot/Patch.cs
@@ -0,0 +1,27 @@
+﻿using PulsarModLoader;
+using HarmonyLib;
+
+namespace CapBot
+{
+    [HarmonyPatch(typeof(PLPlayer), "UpdateAIPriorities")]
+    class Patch
+    {
+        static void Postfix(PLPlayer __instance) 
+        {
+            if (__instance.GetPawn() == null || !__instance.IsBot || __instance.GetClassID() != 0) return;
+            if(__instance.StartingShip != null && __instance.StartingShip.MyStats.GetShipComponent<PLCaptainsChair>(ESlotType.E_COMP_CAPTAINS_CHAIR,false) != null) 
+            {
+                if ((__instance.StartingShip.CaptainsChairPivot.position - __instance.GetPawn().transform.position).sqrMagnitude > 16)
+                {
+                    __instance.MyBot.AI_TargetPos = __instance.StartingShip.CaptainsChairPivot.position;
+                    __instance.MyBot.AI_TargetPos_Raw = __instance.MyBot.AI_TargetPos;
+                    __instance.MyBot.EnablePathing = true;
+                }
+                else if(__instance.StartingShip.CaptainsChairPlayerID != __instance.GetPlayerID())
+                {
+                    __instance.StartingShip.AttemptToSitInCaptainsChair(__instance.GetPlayerID());
+                }
+            }
+        }
+    }
+}
