e717b53c352762dc16a737165c41e92601179f80
Gustavo Ribeiro <pokegustavo@gmail.com>
2021-12-09 20:32:49 -0300

    fixed chair, bot spawns automatically and reset spawn timer on join game

--- CapBot/Patch.cs
+++ CapBot/Patch.cs
@@ -1,27 +1,71 @@
 ﻿using PulsarModLoader;
 using HarmonyLib;
+using UnityEngine;
 
 namespace CapBot
 {
     [HarmonyPatch(typeof(PLPlayer), "UpdateAIPriorities")]
     class Patch
     {
-        static void Postfix(PLPlayer __instance) 
+        static void Postfix(PLPlayer __instance)
         {
             if (__instance.GetPawn() == null || !__instance.IsBot || __instance.GetClassID() != 0) return;
-            if(__instance.StartingShip != null && __instance.StartingShip.MyStats.GetShipComponent<PLCaptainsChair>(ESlotType.E_COMP_CAPTAINS_CHAIR,false) != null) 
+            if (__instance.StartingShip != null && __instance.StartingShip.MyStats.GetShipComponent<PLCaptainsChair>(ESlotType.E_COMP_CAPTAINS_CHAIR, false) != null)
             {
-                if ((__instance.StartingShip.CaptainsChairPivot.position - __instance.GetPawn().transform.position).sqrMagnitude > 16)
+                if ((__instance.StartingShip.CaptainsChairPivot.position - __instance.GetPawn().transform.position).sqrMagnitude > 4)
                 {
                     __instance.MyBot.AI_TargetPos = __instance.StartingShip.CaptainsChairPivot.position;
                     __instance.MyBot.AI_TargetPos_Raw = __instance.MyBot.AI_TargetPos;
                     __instance.MyBot.EnablePathing = true;
                 }
-                else if(__instance.StartingShip.CaptainsChairPlayerID != __instance.GetPlayerID())
+                else if (__instance.StartingShip.CaptainsChairPlayerID != __instance.GetPlayerID())
                 {
                     __instance.StartingShip.AttemptToSitInCaptainsChair(__instance.GetPlayerID());
                 }
+
             }
+        }
+    }
+
+    [HarmonyPatch(typeof(PLController), "Update")]
+    class SitInChair
+    {
+        static void Postfix(PLController __instance)
+        {
+            if (__instance.MyPawn == null || __instance.MyPawn.MyPlayer == null || !__instance.MyPawn.MyPlayer.IsBot || __instance.MyPawn.MyPlayer.GetClassID() != 0 || __instance.MyPawn.MyPlayer.StartingShip == null) return;
+            if (__instance.MyPawn.MyPlayer.StartingShip.CaptainsChairPlayerID == __instance.MyPawn.MyPlayer.GetPlayerID())
+            {
+                PLCaptainsChair shipComponent = __instance.MyPawn.MyPlayer.StartingShip.MyStats.GetShipComponent<PLCaptainsChair>(ESlotType.E_COMP_CAPTAINS_CHAIR, false);
+                if (shipComponent != null && shipComponent.MyInstance != null && shipComponent.MyInstance.MalePawnPivot != null)
+                {
+                    __instance.MyPawn.MyPlayer.GetPawn().transform.position = shipComponent.MyInstance.MalePawnPivot.position;
+                    shipComponent.MyInstance.Rot.transform.rotation = __instance.MyPawn.MyPlayer.GetPawn().HorizontalMouseLook.transform.rotation;
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(PLUIClassSelectionMenu), "Update")]
+    class SpawnBot
+    {
+        public static float delay = 0f;
+        static void Postfix()
+        {
+            if (PLEncounterManager.Instance.PlayerShip != null && PLServer.Instance.GetCachedFriendlyPlayerOfClass(0, PLEncounterManager.Instance.PlayerShip) == null && delay > 3f)
+            {
+                PLServer.Instance.ServerAddCrewBotPlayer(0);
+                PLServer.Instance.GameHasStarted = true;
+            }
+            else if (PLEncounterManager.Instance.PlayerShip != null && PLServer.Instance.GetCachedFriendlyPlayerOfClass(0, PLEncounterManager.Instance.PlayerShip) == null) delay += Time.deltaTime;
+        }
+    }
+
+    [HarmonyPatch(typeof(PLGlobal), "EnterNewGame")]
+    class OnJoin
+    {
+        static void Postfix()
+        {
+            SpawnBot.delay = 0f;
         }
     }
 }
